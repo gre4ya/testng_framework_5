6f6479ac4368c4550ad5dc853c18a1c1f46723cf
Andrii Grechyn <gre4ya@icloud.com>
2023-01-15 14:36:08 -0600

    added new lines of code

--- src/test/java/testng_knowledge/_04_TestDescription.java
+++ src/test/java/testng_knowledge/_04_TestDescription.java
@@ -1,17 +1,15 @@
 package testng_knowledge;
 
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 
 public class _04_TestDescription {
+/*
+    @Test(priority = 1, description = "Login with valid credentials", enabled = true)
+    public void login(){System.out.println("This is login");}
 
-    @Test(priority = 1)
-    public void login(){
-        System.out.println("This is login");
-    }
-    @Test(priority = 2)
-    public void invalidLogin(){
-        System.out.println("This is invalid login");
-    }
-
-
+    @Ignore // Ignores  test
+    @Test(priority = 2, description = "Login with invalid credentials", enabled = false)
+    public void invalidLogin(){System.out.println("This is invalid login");}
+ */
 }
