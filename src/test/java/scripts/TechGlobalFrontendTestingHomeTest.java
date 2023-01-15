8539663e35759a9d5b2838372b6203ea916db646
Andrii Grechyn <gre4ya@icloud.com>
2023-01-15 15:59:37 -0600

    added new lines of code

---
+++ src/test/java/scripts/TechGlobalFrontendTestingHomeTest.java
@@ -0,0 +1,89 @@
+package scripts;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pages.TechGlobalFrontendTestingHomePage;
+import utilities.Waiter;
+
+public class TechGlobalFrontendTestingHomeTest extends TechGlobalBase{
+    /*
+    TEST TASK
+    Go to https://techglobal-training.netlify.app/
+    Click on "Practices" dropdown
+    Click on "Frontend Testing" option
+    Validate below cards are dsiplayed and their text is as below
+    "Locators"
+    "Xpath-CSS Locators"
+    "Dynamic Elements"
+    "Waits"
+    "Dropdowns"
+    "Radio Buttons"
+    "Checkboxes"
+    "Alerts"
+    "IFrames"
+    "Multiple Windows"
+    "Static Tables"
+    "Dynamic Tables"
+    "File Upload"
+    "File Download"
+    "Actions"
+    "Login Form"
+    "Forgot Password"
+    "Sortable Tables"
+    "Pagination"
+    "Calendar"
+     */
+    @BeforeMethod
+    public void setPage(){
+        techGlobalFrontendTestingHomePage = new TechGlobalFrontendTestingHomePage();
+    }
+
+    @Test(priority = 1, description = "Validate Frontend Testing Cards")
+    public void validateFrontendTestingCards(){
+        techGlobalFrontendTestingHomePage.getFrontendTestingPage();
+
+        String[] cardText =
+                {
+                  "Locators",
+                  "Xpath-CSS Locators",
+                  "Dynamic Elements",
+                  "Waits",
+                  "Dropdowns",
+                  "Radio Buttons",
+                  "Checkboxes",
+                  "Alerts",
+                  "IFrames",
+                  "Multiple Windows",
+                  "Static Tables",
+                  "Dynamic Tables",
+                  "File Upload",
+                  "File Download",
+                  "Actions",
+                  "Login Form",
+                  "Forgot Password",
+                  "Sortable Tables",
+                  "Pagination",
+                  "Calendar"
+                };
+        for (int i = 0; i < 20; i++) {
+            Assert.assertTrue(techGlobalFrontendTestingHomePage.cards.get(i).isDisplayed());
+            Assert.assertEquals(techGlobalFrontendTestingHomePage.cards.get(i).getText(),cardText[i]);
+        }
+    }
+
+    /*
+    TEST TASK
+    Go to https://techglobal-training.netlify.app/
+    Click on "Practices" dropdown
+    Click on "Frontend Testing" option
+    Validate the url contains "frontend"
+     */
+
+    @Test(priority = 2, description = "Validate URL")
+    public void validateFrontendTestingURL(){
+        techGlobalFrontendTestingHomePage.getFrontendTestingPage();
+        Assert.assertTrue(driver.getCurrentUrl().contains("frontend"));
+    }
+
+}
