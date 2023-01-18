c4dee47ebd85ca5ecd93d76c2aeab5304e3aa601
Andrii Grechyn <gre4ya@icloud.com>
2023-01-17 19:24:57 -0600

    added new lines of code

--- src/test/java/scripts/TechGlobalDynamicElementsTest.java
+++ src/test/java/scripts/TechGlobalDynamicElementsTest.java
@@ -1,13 +1,23 @@
 package scripts;
 
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pages.TechGlobalDynamicElementsPage;
 import pages.TechGlobalFrontendTestingHomePage;
 
 public class TechGlobalDynamicElementsTest extends TechGlobalBase{
+    /*
+    TEST CASES
+    Dynamic Elements
+    Go to https://techglobal-training.netlify.app/
+    Click on "Practices" dropdown in the header
+    Select the "Frontend Testing" option
+    Click on the "Dynamic Elements" card
+    Validate both boxes are displayed with below texts
+    Box 1
+    Box 2
+     */
     @BeforeMethod
     public void setPage(){
         techGlobalDynamicElementsPage = new TechGlobalDynamicElementsPage();
@@ -22,17 +32,8 @@ public class TechGlobalDynamicElementsTest extends TechGlobalBase{
 
     Assert.assertTrue(techGlobalDynamicElementsPage.box1.isDisplayed());
     Assert.assertEquals(techGlobalDynamicElementsPage.box1.getText(), "Box 1");
-    }
-
-    @Test(priority = 2, description = "Testing Dynamic Web Elements")
-    public void dynamicElementsTest2(){
-
-    techGlobalFrontendTestingHomePage.getFrontendTestingPage();
-    techGlobalFrontendTestingHomePage.clickOnCard(2);
 
     Assert.assertTrue(techGlobalDynamicElementsPage.box2.isDisplayed());
     Assert.assertEquals(techGlobalDynamicElementsPage.box2.getText(), "Box 2");
     }
-
-
 }
