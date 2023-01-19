aec2f8d97f4399e2487da111467418fd8cd473c7
Andrii Grechyn <gre4ya@icloud.com>
2023-01-18 20:45:05 -0600

    added new lines of code

--- src/test/java/scripts/TechGlobalCheckBoxTest.java
+++ src/test/java/scripts/TechGlobalCheckBoxTest.java
@@ -5,6 +5,7 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pages.TechGlobalCheckBoxPage;
 import pages.TechGlobalFrontendTestingHomePage;
+import utilities.Waiter;
 
 public class TechGlobalCheckBoxTest extends TechGlobalBase{
 
@@ -24,8 +25,8 @@ public class TechGlobalCheckBoxTest extends TechGlobalBase{
     Deselect bpth and validate they are deselected
     */
 
-    @Test(priority = 1, description = "Validate Checkboxes")
-    public void ValidateCheckboxes(){
+    @Test(priority = 1, description = "Validate Checkboxes (1)")
+    public void ValidateCheckboxes1(){
         techGlobalFrontendTestingHomePage.getFrontendTestingPage();
         techGlobalFrontendTestingHomePage.clickOnCard(6);
         for (int i = 0; i < techGlobalCheckBoxPage.checkBoxLabel1.size(); i++) {
@@ -35,12 +36,43 @@ public class TechGlobalCheckBoxTest extends TechGlobalBase{
         }
         for (int i = 0; i < techGlobalCheckBoxPage.checkBoxLabel1.size(); i++) {
             techGlobalCheckBoxPage.checkBoxLabel1.get(i).click();
+            Waiter.pause(1);
             Assert.assertTrue(techGlobalCheckBoxPage.checkBoxInput1.get(i).isSelected());
         }
         for (int i = 0; i < techGlobalCheckBoxPage.checkBoxLabel1.size(); i++) {
             techGlobalCheckBoxPage.checkBoxLabel1.get(i).click();
+            Waiter.pause(1);
             Assert.assertFalse(techGlobalCheckBoxPage.checkBoxInput1.get(i).isSelected());
         }
+    }
 
+    @Test(priority = 1, description = "Validate Checkboxes (2)")
+    public void ValidateCheckboxes2(){
+        techGlobalFrontendTestingHomePage.getFrontendTestingPage();
+        techGlobalFrontendTestingHomePage.clickOnCard(6);
+
+        techGlobalCheckBoxPage.checkBoxInput2.forEach(box -> {
+            Assert.assertTrue(box.isDisplayed());
+            Assert.assertFalse(box.isSelected());
+            Assert.assertTrue(box.isEnabled());
+        });
+
+//        for (int i = 0; i < techGlobalCheckBoxPage.checkBoxInput2.size(); i++) {
+//            Assert.assertTrue(techGlobalCheckBoxPage.checkBoxInput2.get(i).isDisplayed());
+//            Assert.assertFalse(techGlobalCheckBoxPage.checkBoxInput2.get(i).isSelected());
+//            Assert.assertTrue(techGlobalCheckBoxPage.checkBoxInput2.get(i).isEnabled());
+//        }
+
+
+        for (int i = 0; i < techGlobalCheckBoxPage.checkBoxInput2.size(); i++) {
+            techGlobalCheckBoxPage.checkBoxLabel2.get(i).click();
+            Waiter.pause(1);
+            Assert.assertTrue(techGlobalCheckBoxPage.checkBoxInput2.get(i).isSelected());
+        }
+        for (int i = 0; i < techGlobalCheckBoxPage.checkBoxInput2.size(); i++) {
+            techGlobalCheckBoxPage.checkBoxLabel2.get(i).click();
+            Waiter.pause(1);
+            Assert.assertFalse(techGlobalCheckBoxPage.checkBoxInput2.get(i).isSelected());
+        }
     }
 }
