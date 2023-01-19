cf79884151f513a6f098ed0f9d3e08af45b5f208
Andrii Grechyn <gre4ya@icloud.com>
2023-01-18 20:55:50 -0600

    added new lines of code

--- src/test/java/scripts/TechGlobalCheckBoxTest.java
+++ src/test/java/scripts/TechGlobalCheckBoxTest.java
@@ -22,13 +22,15 @@ public class TechGlobalCheckBoxTest extends TechGlobalBase{
     Click on the "Checkboxes" card
     Validate "Apple" and "Microsoft" checkboxes are displayed, enabled and not selected
     Select both and validate they are both selected
-    Deselect bpth and validate they are deselected
+    Deselect both and validate they are deselected
     */
 
-    @Test(priority = 1, description = "Validate Checkboxes (1)")
+    @Test(priority = 1, description = "Validate Checkboxes first group")
     public void ValidateCheckboxes1(){
+
         techGlobalFrontendTestingHomePage.getFrontendTestingPage();
         techGlobalFrontendTestingHomePage.clickOnCard(6);
+
         for (int i = 0; i < techGlobalCheckBoxPage.checkBoxLabel1.size(); i++) {
             Assert.assertTrue(techGlobalCheckBoxPage.checkBoxInput1.get(i).isDisplayed());
             Assert.assertFalse(techGlobalCheckBoxPage.checkBoxInput1.get(i).isSelected());
@@ -36,18 +38,29 @@ public class TechGlobalCheckBoxTest extends TechGlobalBase{
         }
         for (int i = 0; i < techGlobalCheckBoxPage.checkBoxLabel1.size(); i++) {
             techGlobalCheckBoxPage.checkBoxLabel1.get(i).click();
-            Waiter.pause(1);
+            //Waiter.pause(1);
             Assert.assertTrue(techGlobalCheckBoxPage.checkBoxInput1.get(i).isSelected());
         }
         for (int i = 0; i < techGlobalCheckBoxPage.checkBoxLabel1.size(); i++) {
             techGlobalCheckBoxPage.checkBoxLabel1.get(i).click();
-            Waiter.pause(1);
+            //Waiter.pause(1);
             Assert.assertFalse(techGlobalCheckBoxPage.checkBoxInput1.get(i).isSelected());
         }
     }
 
-    @Test(priority = 1, description = "Validate Checkboxes (2)")
+    /*
+    Go to https://techglobal-training.netlify.app/
+    Click on "Practices" dropdown in the header
+    Select the "Frontend Testing" option
+    Click on the "Checkboxes" card
+    Validate "Tesla" and "SpaceX" checkboxes are displayed, enabled and not selected
+    Select both and validate they are both selected
+    Deselect both and validate they are deselected (edited)
+     */
+
+    @Test(priority = 1, description = "Validate Checkboxes second group")
     public void ValidateCheckboxes2(){
+
         techGlobalFrontendTestingHomePage.getFrontendTestingPage();
         techGlobalFrontendTestingHomePage.clickOnCard(6);
 
@@ -57,21 +70,20 @@ public class TechGlobalCheckBoxTest extends TechGlobalBase{
             Assert.assertTrue(box.isEnabled());
         });
 
-//        for (int i = 0; i < techGlobalCheckBoxPage.checkBoxInput2.size(); i++) {
-//            Assert.assertTrue(techGlobalCheckBoxPage.checkBoxInput2.get(i).isDisplayed());
-//            Assert.assertFalse(techGlobalCheckBoxPage.checkBoxInput2.get(i).isSelected());
-//            Assert.assertTrue(techGlobalCheckBoxPage.checkBoxInput2.get(i).isEnabled());
-//        }
-
+//      for (int i = 0; i < techGlobalCheckBoxPage.checkBoxInput2.size(); i++) {
+//          Assert.assertTrue(techGlobalCheckBoxPage.checkBoxInput2.get(i).isDisplayed());
+//          Assert.assertFalse(techGlobalCheckBoxPage.checkBoxInput2.get(i).isSelected());
+//          Assert.assertTrue(techGlobalCheckBoxPage.checkBoxInput2.get(i).isEnabled());
+//      }
 
         for (int i = 0; i < techGlobalCheckBoxPage.checkBoxInput2.size(); i++) {
             techGlobalCheckBoxPage.checkBoxLabel2.get(i).click();
-            Waiter.pause(1);
+            //Waiter.pause(1);
             Assert.assertTrue(techGlobalCheckBoxPage.checkBoxInput2.get(i).isSelected());
         }
         for (int i = 0; i < techGlobalCheckBoxPage.checkBoxInput2.size(); i++) {
             techGlobalCheckBoxPage.checkBoxLabel2.get(i).click();
-            Waiter.pause(1);
+            //Waiter.pause(1);
             Assert.assertFalse(techGlobalCheckBoxPage.checkBoxInput2.get(i).isSelected());
         }
     }
--- src/test/java/scripts/TechGlobalWaitsTest.java
+++ src/test/java/scripts/TechGlobalWaitsTest.java
@@ -2,18 +2,20 @@ package scripts;
 
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import pages.TechGlobalFrontendTestingHomePage;
 import pages.TechGlobalWaitsPage;
 import utilities.Waiter;
 
 public class TechGlobalWaitsTest extends TechGlobalBase {
-
+    @Ignore
     @BeforeMethod
     public void setPage(){
         techGlobalWaitsPage = new TechGlobalWaitsPage();
         techGlobalFrontendTestingHomePage = new TechGlobalFrontendTestingHomePage();
     }
+    @Ignore
     @Test(priority = 1, description = "Validation red box is visible")
     public void validateRedBox(){
         techGlobalFrontendTestingHomePage.getFrontendTestingPage();
