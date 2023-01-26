2c8841b1fb36b5beb3edc5be09ee0267f0444d15
Andrii Grechyn <gre4ya@icloud.com>
2023-01-25 19:14:56 -0600

    added new lines of code to testng_framework_5 project

--- src/test/java/testng_knowledge/_09_Parallel_Test.java
+++ src/test/java/testng_knowledge/_09_Parallel_Test.java
@@ -3,26 +3,24 @@ package testng_knowledge;
 import org.testng.annotations.Test;
 import utilities.Waiter;
 
-public class _08_Parallel_Test {
+public class _09_Parallel_Test {
 
     @Test(enabled = false)
     public void method1(){
         Waiter.pause(5);
-        System.out.println("This is method 1 in the 08 class");
+        System.out.println("This is method 1 in the 09 class");
         System.out.println("Thread = " + Thread.currentThread().getId());
     }
     @Test(enabled = false)
     public void method2(){
         Waiter.pause(4);
-        System.out.println("This is method 2 in the 08 class");
+        System.out.println("This is method 2 in the 09 class");
         System.out.println("Thread = " + Thread.currentThread().getId());
     }
     @Test(enabled = false)
     public void method3(){
         Waiter.pause(6);
-        System.out.println("This is method 3 in the 08 class");
+        System.out.println("This is method 3 in the 09 class");
         System.out.println("Thread = " + Thread.currentThread().getId());
     }
-
-
 }
