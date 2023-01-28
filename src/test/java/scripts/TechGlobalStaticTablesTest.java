2a1b989841162f2ee7e4076c4c6194f2e079fde7
Andrii Grechyn <gre4ya@icloud.com>
2023-01-28 15:39:55 -0600

    added new lines of code in testng_framework_5 project

--- src/test/java/scripts/TechGlobalStaticTablesTest.java
+++ src/test/java/scripts/TechGlobalStaticTablesTest.java
@@ -1,11 +1,14 @@
 package scripts;
 
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pages.TechGlobalFrontendTestingHomePage;
 import pages.TechGlobalStaticTablesPage;
+import utilities.TableData;
 
+import java.util.List;
 import java.util.stream.IntStream;
 
 public class TechGlobalStaticTablesTest extends TechGlobalBase{
@@ -27,13 +30,12 @@ public class TechGlobalStaticTablesTest extends TechGlobalBase{
      Check that the headers of the table are “Rank”, “Company”, “Employees”, and “Country”
      Verify the rows of the table
      */
-    @Test(priority = 1)
+    @Test(priority = 1, description = "Validate Header Table text")
     public void validateStaticTablesHeader(){
         String[] headers = {"Rank", "Company", "Employees", "Country"};
         IntStream.range(0, headers.length).forEach(i ->
                 Assert.assertEquals(techGlobalStaticTablesPage.tableHeader.get(i).getText(), headers[i]));
     }
-
     /**
      TEST 2
      Go to https://techglobal-training.netlify.app/
@@ -43,11 +45,26 @@ public class TechGlobalStaticTablesTest extends TechGlobalBase{
      Check that first row of the table has the values “1", “Amazon”, “1,523,000", and “USA”
      Verify the columns of the table
      */
-    @Test(priority = 2)
+    @Test(priority = 2, description = "Validate Row 1 text")
     public void validateStaticTablesRow1(){
         String[] row1 = {"1", "Amazon", "1,523,000", "USA"};
-        IntStream.range(0, row1.length).forEach(i ->
-                Assert.assertEquals(techGlobalStaticTablesPage.row1Elements.get(i).getText(), row1[i]));
+        List<WebElement> tableRow = TableData.getTableRow(driver, 1);
+        IntStream.range(0, row1.length).forEach(i -> Assert.assertEquals(tableRow.get(i).getText(), row1[i]));
     }
-
+    /**
+     TEST 3
+     Go to https://techglobal-training.netlify.app/
+     Click on “Practices” dropdown in the header
+     Select the “Frontend Testing” option
+     Click on the “Static Tables” card
+     Check that the “Country” column of the table has the values “USA”, “China”, “USA”, “USA”, “S. Korea”
+     Verify the entire table
+     */
+    @Test(priority = 3, description = "Validate Column 4 text")
+    public void validateStaticTableColumn4(){
+        String[] column4Expected = {"USA", "China", "USA", "USA", "S. Korea"};
+        List<WebElement> column4Actual = TableData.getTableColumn(driver, 4);
+        IntStream.range(0, column4Actual.size()).forEach(i ->
+                Assert.assertEquals(column4Actual.get(i).getText(), column4Expected[i]));
+    }
 }
