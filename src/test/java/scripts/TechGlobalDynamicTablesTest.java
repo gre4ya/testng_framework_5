60dfb08150e95473c69f279fe193c9ed1234ba44
Andrii Grechyn <gre4ya@icloud.com>
2023-01-29 11:56:55 -0600

    added new lines of code in testng_framework_5 project

--- src/test/java/scripts/TechGlobalDynamicTablesTest.java
+++ src/test/java/scripts/TechGlobalDynamicTablesTest.java
@@ -5,6 +5,9 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pages.TechGlobalDynamicTablesPage;
 import pages.TechGlobalFrontendTestingHomePage;
+import utilities.TableData;
+
+import java.util.stream.IntStream;
 
 public class TechGlobalDynamicTablesTest extends TechGlobalBase{
 
@@ -28,4 +31,43 @@ public class TechGlobalDynamicTablesTest extends TechGlobalBase{
     public void popupValidation(){techGlobalDynamicTablesPage.addProductButton.click();
        Assert.assertEquals(techGlobalDynamicTablesPage.modalCardTitle.getText(), "Add New Product");
     }
+
+    /**
+     DYNAMIC TABLES
+     TEST1
+     Go to https://techglobal-training.netlify.app/
+     Click on “Practices” dropdown in the header
+     Select the “Frontend Testing” option
+     Click on the “Dynamic Tables” card
+     Click the “Add Product” button
+     Enter “2” in the Quantity field
+     Enter “Apple Watch” in the Product field
+     Enter “500” in the Price $ field
+     Click the “Submit” button
+     Verify that a new row has been added to the table with the values “2”,
+     “Apple Watch, “500” and the calculated Total $ value is “1,000"
+     Verify that the Total $ value at the bottom of the table has been updated
+     to reflect the total cost of all the products in the table, including the newly added one.
+     */
+    @Test(priority = 1, description = "Validate Dynamic Table")
+    public void validateDynamicTable(){
+        int initialTotal = Integer.parseInt(techGlobalDynamicTablesPage.totalAmount.getText().replaceAll("[^0-9]", ""));
+        techGlobalDynamicTablesPage.addProductButton.click();
+        Assert.assertEquals(techGlobalDynamicTablesPage.modalCardTitle.getText(), "Add New Product");
+        String[] products = {"2", "Apple Watch", "500"};
+        int myProductTotal = Integer.parseInt(products[0]) * Integer.parseInt(products[2]);
+        int initialTableSize = techGlobalDynamicTablesPage.tableRows.size();
+        Assert.assertEquals(initialTableSize, 3);
+
+        IntStream.range(0, products.length).forEach(i -> techGlobalDynamicTablesPage.productDetails.get(i).sendKeys(products[i]));
+        techGlobalDynamicTablesPage.submitButton.click();
+
+        Assert.assertEquals(techGlobalDynamicTablesPage.tableRows.size(), initialTableSize + 1);
+
+        int productTotal = Integer.parseInt(TableData.getTableRow(driver, 4).get(3).getText().replaceAll("[^0-9]", ""));
+
+
+
+    }
+
 }
