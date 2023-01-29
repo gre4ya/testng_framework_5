a628e27f7d37f5e2755077c8fd1e8b7fa094a071
Andrii Grechyn <gre4ya@icloud.com>
2023-01-29 14:47:53 -0600

    added new lines of code in testng_framework_5 project

---
+++ src/test/java/scripts/TechGlobalCalendarTest.java
@@ -0,0 +1,40 @@
+package scripts;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pages.TechGlobalCalendarPage;
+import pages.TechGlobalFrontendTestingHomePage;
+
+public class TechGlobalCalendarTest extends TechGlobalBase{
+
+    @BeforeMethod
+    public void setPage(){
+        techGlobalFrontendTestingHomePage = new TechGlobalFrontendTestingHomePage();
+        techGlobalCalendarPage = new TechGlobalCalendarPage();
+        techGlobalFrontendTestingHomePage.getFrontendTestingPage();
+        techGlobalFrontendTestingHomePage.clickOnCard("Calendar");
+    }
+
+    /**
+     CALENDAR - DATE PICKER
+     Go to https://techglobal-training.netlify.app/
+     Click on "Practices" dropdown in the header
+     Select the "Frontend Testing" option
+     Click on the "Calendar" card
+     Click on the calendar input field to open the date picker
+     Click the next button until the year 2024 is displayed
+     Click on the month button until May
+     Click on a specific date, for example the 16th
+     Validate that the text displayed on the result is "You have selected Thu May 16 2024."
+     */
+    @Test(priority = 1, description = "Validate Date Picker")
+    public void validateDatePicker(){
+        techGlobalCalendarPage.calendarButton.click();
+
+
+
+    }
+
+
+
+}
